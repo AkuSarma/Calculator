5531af2096848f9db29d0fd26257ab2df671c60c
Aku Sarma <akusarma1702@gmail.com>
2021-01-12 23:19:53 +0530

    completed the trigonometric functions

--- calculations.py
+++ calculations.py
@@ -20,6 +20,21 @@ class Cal:
 
                 self.lis.append(math.sqrt(int(separateditem)))
 
+            elif self.signUsed == "sin":
+                separateditem = separatedList[0].replace("sin","")
+
+                self.lis.append(math.sin(int(separateditem)))
+
+            elif self.signUsed == "cos":
+                separateditem = separatedList[0].replace("cos","")
+
+                self.lis.append(math.cos(int(separateditem)))
+
+            elif self.signUsed == "tan":
+                separateditem = separatedList[0].replace("tan","")
+
+                self.lis.append(math.tan(int(separateditem)))
+
         if self.signUsed in separatedList[1]:
             if self.signUsed == "!":
                 separateditem = separatedList[0][:-1]
@@ -31,13 +46,28 @@ class Cal:
 
                 self.lis.append(math.sqrt(int(separateditem)))
 
+            elif self.signUsed == "sin":
+                separateditem = separatedList[0].replace("sin","")
+
+                self.lis.append(math.sin(int(separateditem)))
+
+            elif self.signUsed == "cos":
+                separateditem = separatedList[0].replace("cos","")
+
+                self.lis.append(math.cos(int(separateditem)))
+
+            elif self.signUsed == "tan":
+                separateditem = separatedList[0].replace("tan","")
+
+                self.lis.append(math.tan(int(separateditem)))
+
         if self.signUsed not in separatedList[0]:
             self.lis.append(separatedList[0])
 
         if self.signUsed not in separatedList[1]:
             self.lis.append(separatedList[1])
 
-    def calculations(self):
+    def calculator(self):
         if self.mathSignUsed == "+":
             self.answer = int(self.lis[0]) + int(self.lis[1])
 
@@ -52,7 +82,10 @@ class Cal:
 
 
     def result(self):
-        self.separator()
-        self.calculations()
-
-        return self.answer
+        try:
+            self.separator()
+            self.calculator()
+        except Exception as e:
+            return "error"
+        else:
+            return self.answer
